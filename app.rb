732a57433cb7e330f6914dd49d391f8e1b35b857
Min Kim <mk4555@nyu.edu>
2018-01-13 20:32:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ class App < Sinatra::Base
   post '/team' do
     @team_name = params[:team_name]
     @coach = params[:coach]
-    @
+    @pg = params[:point_guard]
+    @sg = params[:shooting_guard]
+    @sf = params[:small_forward]
+    @pf = params[:power_forward]
+    @c = params[:center]
     erb :team
   end
 end
